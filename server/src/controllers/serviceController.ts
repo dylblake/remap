83176498b307b97d3ba4ca2e3be1f647c50f5a5c
Dylan Evans <dylbalke03@gmail.com>
2024-08-20 08:52:53 -0400

    Updating ServiceController

--- server/src/controllers/serviceController.ts
+++ server/src/controllers/serviceController.ts
@@ -2,16 +2,25 @@ import { z } from 'zod';
 import { Request, Response } from 'express';
 import pool from '../config/db';
 import { serviceSchema } from '../models/serviceModel';
+import { generateUUID } from '../utils/uuid';
 
 export const createService = async (req: Request, res: Response) => {
   try {
-    const serviceData = serviceSchema.parse(req.body);
+    // Generate a UUID for the new service
+    const newServiceId = generateUUID();
 
-    const { serviceName } = serviceData;
+    // Validate and parse the incoming request data
+    const serviceData = serviceSchema.parse({
+      ...req.body,
+      uuid: newServiceId, // Add the generated UUID to the request data
+    });
 
+    const { uuid, name, tier, upperServiceId, middleServiceId } = serviceData;
+
+    // Insert the new service into the database
     const newService = await pool.query(
-      'INSERT INTO services (name, email, age) VALUES ($1, $2, $3) RETURNING *',
-      [serviceName]
+      'INSERT INTO services (uuid, name, tier, upper_service_id, middle_service_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, NOW(), NOW()) RETURNING *',
+      [uuid, name, tier, upperServiceId, middleServiceId]
     );
 
     res.status(201).json(newService.rows[0]);
@@ -26,8 +35,8 @@ export const createService = async (req: Request, res: Response) => {
 
 export const getServices = async (_req: Request, res: Response) => {
   try {
-    const users = await pool.query('SELECT * FROM services');
-    res.status(200).json(users.rows);
+    const services = await pool.query('SELECT * FROM services');
+    res.status(200).json(services.rows);
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
   }
