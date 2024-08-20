9166e9f1c911ee1a86d167a98b69663a394c455a
Dylan Evans <dylbalke03@gmail.com>
2024-08-19 22:16:45 -0400

    Updating PrimaryServiceModel

--- server/src/models/PrimaryServiceModel.ts
+++ server/src/models/PrimaryServiceModel.ts
@@ -6,8 +6,6 @@ export const primaryServiceSchema = baseSchema.extend({
   serviceName: z.string().min(2, "Name must be at least 2 characters long"),
   primaryServiceId: z.string().uuid(),
   secondaryServiceId: z.string().uuid(),
-  email: z.string().email("Invalid email address"),
-  age: z.number().optional(),
 });
 
 export type User = z.infer<typeof primaryServiceSchema>;
