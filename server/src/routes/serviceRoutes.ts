81863978f56da40b964827e5d859a1f755870d16
Dylan Evans <dylbalke03@gmail.com>
2024-08-20 08:55:52 -0400

    Updating serviceRoutes

--- server/src/routes/serviceRoutes.ts
+++ server/src/routes/serviceRoutes.ts
@@ -1,10 +1,21 @@
-// src/routes/userRoutes.ts
 import { Router } from 'express';
-import { getServices, createService } from '../controllers/serviceController';
+import { getServices, createService, getServiceById, updateService, deleteService } from '../controllers/serviceController';
 
-const router = Router();
+const serviceRoutes = Router();
 
-router.post('/', createService);
-router.get('/', getServices);
+// Route to create a new service
+serviceRoutes.post('/', createService);
 
-export default router;
+// Route to get all services
+serviceRoutes.get('/', getServices);
+
+// Route to get a single service by ID
+serviceRoutes.get('/:id', getServiceById);
+
+// Route to update a service by ID
+serviceRoutes.put('/:id', updateService);
+
+// Route to delete a service by ID
+serviceRoutes.delete('/:id', deleteService);
+
+export default serviceRoutes;
