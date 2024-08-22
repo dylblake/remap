dd4ea86c708c5b0146abbe6817e266474f3aa991
Dylan Evans <dylbalke03@gmail.com>
2024-08-22 10:33:22 -0400

    updating all packages and configs

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,14 @@
+import 'vite/client'
+
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+    VITE_API_BASE_URL: string;
+    // Add other environment variables here if needed
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  
