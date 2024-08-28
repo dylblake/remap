08598f2dabcc5d26f7995ce7bc69c89844341bf0
Dylan Evans <dylbalke03@gmail.com>
2024-08-28 13:53:59 -0400

    Include correct local port in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
@@ -8,5 +8,8 @@ export default defineConfig({
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
     },
+  },
+  server: {
+    port: 5176
   },
 });
