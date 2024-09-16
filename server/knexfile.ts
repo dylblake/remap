a050653ecfcf90aa062a0913366eed31a6b9f591
Dylan Evans <dylbalke03@gmail.com>
2024-09-16 15:13:43 -0400

    Running migrations in init - server/src/app.ts

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,13 +1,38 @@
-require('dotenv').config(); // Load environment variables from .env
+import { Knex } from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env
 
 console.log('Database Name:', process.env.DB_NAME); // Check if DB_NAME is correctly loaded
 console.log(JSON.stringify(process.env, null, 2)); // Print all environment variables
-module.exports = {
+
+const knexConfig: { [key: string]: Knex.Config } = {
   development: {
     client: "pg",
     connection: {
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    pool: {
+      min: 2,
+      max: 20,
+    },
+    migrations: {
+      directory: "./migrations",
+      extension: "ts",
+    },
+    seeds: {
+      directory: './seeds', // Path to seeds directory
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
@@ -25,3 +50,5 @@ module.exports = {
     },
   },
 };
+
+export default knexConfig;
