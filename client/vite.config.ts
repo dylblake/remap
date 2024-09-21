c26527110703c1484a8519c2fc1f5560e672d35f
Dylan Evans <dylbalke03@gmail.com>
2024-09-21 16:50:48 -0400

    Updating config for photos to import via aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
     },
   },
   server: {
-    port: 5176
+    port: 5176,
   },
 });
